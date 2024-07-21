7b6368129049ac523ab535d893dc43fb6054cf2a
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-07-21 19:00:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo docker ps
 ollama pull llama2
 
 # Install prerequisites for Stable Diffusion
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git pip
 
 # Install Stable Diffusion
 curl https://pyenv.run | bash
