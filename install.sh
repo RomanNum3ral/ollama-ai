8366e69022d43a624d39f9047529c71d14abd9b6
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-07-21 13:49:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 # install ollama
 sudo curl -fsSL https://ollama.com/install.sh | sh
-sudo ollama pull llama2
 
 # give it a web ui through Docker
 sudo apt-get update
@@ -16,7 +15,28 @@ echo \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Install the Docker container
 sudo docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 sudo docker ps
+# Add llama2 language model
+ollama pull llama2
+
+# Install prerequisites for Stable Diffusion
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+
+# Install Stable Diffusion
+curl https://pyenv.run | bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+source .bashrc
+pyenv install 3.10
+pyenv global 3.10
+mkdir stablediff
+cd stablediff
+wget https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
+sudo chmod +x webui.sh
+./webui.sh --listen --api
